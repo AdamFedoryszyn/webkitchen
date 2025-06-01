5b38c3bb4e3fa1b9bd330153f8bbe5128133f9bf
Adam Fedoryszyn <adamfedoryszyn@gmail.com>
2025-06-01 19:41:12 +0200

    Added header tag to header component

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -5,12 +5,14 @@ import Container from "../core/container"
 
 const Header = () => {
   return (
-    <Container className="flex justify-between">
+    <header className="sticky top-0 bg-background">    
+      <Container className="flex justify-between">
                 <Logo />
                 <Button asChild>
                   <Link href="/projects">Launch</Link>
                 </Button>
-    </Container>
+      </Container>
+    </header>
   )
 }
 
